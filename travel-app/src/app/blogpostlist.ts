3bc8b8fd0dc60130a1d41e18432c19fc5d20ddd2
philipm92 <philip.mahlberg@hotmail.com>
2021-07-17 14:46:11 +0200

    changed images and did some styling changes

--- travel-app/src/app/blogpostlist.ts
+++ travel-app/src/app/blogpostlist.ts
@@ -12,7 +12,7 @@ export class Locations extends Place  {
 
 // Add Locations for blog post section
 new Locations("St. Charles Church", "Karlsplatz 1", "Vienna", 1010, "St Charles Church_new.jpg", RandomDate());
-new Locations("Zoo Vienna", "Maxingstra&szlig;e 13b", "Vienna", 1130, "Zoo Vienna_new.jpg", RandomDate());
+new Locations("Zoo Vienna", "Maxingstraße 13b", "Vienna", 1130, "Zoo Vienna_new.jpg", RandomDate());
 new Locations("Spielraum", "Otto-Bauer-Gasse 17", "Vienna", 1060, "Spielraum_new.jpg", RandomDate());
 new Locations("Area52", "Franklinstrasse 20/9/R1", "Vienna", 1210, "Area52_new.jpg", RandomDate());
 
